b5ec50a02bbc85395e3005ab648987463af1bd3f
bhyeon1 <byung2587@naver.com>
2025-09-15 21:23:35 +0900

    Delete motion_game_ver2 directory

--- motion_game_ver2/scenes/__init_.py
+++
@@ -1,3 +0,0 @@
-# scenes/__init__.py
-from .mode_select_scene import ModeSelectScene
-__all__ = ["ModeSelectScene"]
--- motion_game_ver2/widgets/__init__.py
+++
@@ -1,3 +0,0 @@
-# widgets/__init__.py
-from .start_hold_widget import StartHoldWidget
-__all__ = ["StartHoldWidget"]
